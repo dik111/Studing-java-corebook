d8bd09ee043e581b1e66a209fd9712a923186d79
11007166 <195812208@qq.com>
2020-03-28 22:57:20 +0800

    :tada: init project

---
+++ src/com/javademo/l3p4/variable/CreateVariable.java
@@ -0,0 +1,17 @@
+package com.javademo.l3p4.variable;
+
+/**
+ * Desription:
+ *
+ * @ClassName CreateVariable
+ * @Author Zhanyuwei
+ * @Date 2020/3/28 22:51
+ * @Version 1.0
+ **/
+public class CreateVariable {
+    public static void main(String[] args) {
+        int vacationDays;
+        vacationDays = 12;
+
+    }
+}
